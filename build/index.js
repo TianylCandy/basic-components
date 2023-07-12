3e602497e0d383d00c9e90cb552955c1891a0dda
tianyuanlin <candytian@moonton.com>
2023-07-12 22:24:07 +0800

    0.0.2

--- build/index.js
+++ build/index.js
@@ -12,7 +12,7 @@ var TextView = function TextView(_a) {
   var children = _a.children;
   return /*#__PURE__*/React__default["default"].createElement("div", {
     className: "text-view"
-  }, children);
+  }, "\u7ED9\u4E2A\u54C8\u54C8\u54C8", children);
 };
 
 /*! *****************************************************************************
